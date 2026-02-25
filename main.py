745e3329303d7672c064c9a1dd0d35af67e683f5
roberta-barros <robertabarrost@gmail.com>
2026-02-25 11:50:13 -0300

    roteiro 0.0

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    raise Exception()
+
+s = re.sub(r"\s+", "", sys.argv[1])  
+
+if not re.fullmatch(r"\d+(?:[+-]\d+)*", s):
+    raise Exception()
+
+nums = list(map(int, re.findall(r"\d+", s)))
+ops = re.findall(r"[+-]", s)
+
+res = nums[0]
+for op, num in zip(ops, nums[1:]):
+    res = res + num if op == "+" else res - num
+
+print(res, end="")
